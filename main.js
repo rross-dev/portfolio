8d3d67768f99c06e69393c15a9819d98323c8792
rross-dev <ryanross.dev@gmail.com>
2023-06-27 21:00:06 -0500

    inital commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// Vanilla JavaScript
+window.addEventListener('load', function () {
+  setTimeout(function () {
+    document.getElementById('loading').classList.add('hide');
+  }, 1000); // 2000 milliseconds = 2 seconds
+});
+
+// Vanilla JavaScript
+window.addEventListener('load', function () {
+  setTimeout(function () {
+    document.getElementById('loading-inv').classList.add('hide');
+  }, 1000); // 2000 milliseconds = 2 seconds
+});
