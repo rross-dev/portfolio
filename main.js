2e770da45f81ddc93293ee6c3a3bf0501f822ce6
rross-dev <ryanross.dev@gmail.com>
2023-08-22 00:48:32 -0500

    page additions and styling tweaks

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ window.addEventListener('load', function () {
 $(document).ready(function() {
     const projectThumbnails = document.querySelectorAll('.project-thumbnails img');
     const projectThumbnails2 = document.querySelectorAll('.project-thumbnails2 img');
+    const projectThumbnails4 = document.querySelectorAll('.project-thumbnails4 img');
     const rsImg = document.querySelectorAll('.right-side img');
     const fullscreenOverlay = document.querySelector('.fullscreen-overlay');
     const fullscreenImage = document.querySelector('.fullscreen-image');
@@ -29,6 +30,14 @@ $(document).ready(function() {
     });
 
     projectThumbnails2.forEach(thumbnail => {
+        thumbnail.addEventListener('click', () => {
+            const thumbnailSrc = thumbnail.getAttribute('src');
+            fullscreenImage.setAttribute('src', thumbnailSrc);
+            fullscreenOverlay.style.display = 'block';
+        });
+    });
+
+    projectThumbnails4.forEach(thumbnail => {
         thumbnail.addEventListener('click', () => {
             const thumbnailSrc = thumbnail.getAttribute('src');
             fullscreenImage.setAttribute('src', thumbnailSrc);
